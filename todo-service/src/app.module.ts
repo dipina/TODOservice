168bf703cafeed9fcec948f13b1c13205fd7bdb5
dipina <dipina@deusto.es>
2024-11-12 07:22:53 +0100

    now got together in same folder front-end and back-end

--- todo-service/src/app.module.ts
+++ todo-service/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [
+    TodosModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'], // Ensures that API routes are not affected by static file serving
+    }),
+  ],
 })
 export class AppModule {}
--- todo-service/src/main.ts
+++ todo-service/src/main.ts
@@ -1,16 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // Serve static files from the public folder
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   
   // Enable CORS for localhost:3000
+  /*
   app.enableCors({
     origin: 'http://localhost:3000',  // Allow requests only from localhost:3000
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     credentials: true, // Include credentials like cookies in requests
-  });
+  });*/
 
-  await app.listen(4000); // Assuming your NestJS app runs on a different port
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
+
+
